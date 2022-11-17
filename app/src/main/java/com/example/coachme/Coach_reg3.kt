5997077cb22505c7374d9b1b3115a8eff77a0969
JasonNCF <61649503+JasonNCF@users.noreply.github.com>
2022-11-18 03:44:00 +0800

    Altered Coach_reg3

--- app/src/main/java/com/example/coachme/Coach_reg3.kt
+++ app/src/main/java/com/example/coachme/Coach_reg3.kt
@@ -1,4 +1,4 @@
-package com.example.coachme
+package com.example.comp3330project
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -46,26 +46,14 @@ class Coach_reg3 : AppCompatActivity() {
 
         val cont = findViewById<Button>(R.id.contd)
         cont.setOnClickListener(View.OnClickListener() {
-            qua = findViewById<EditText>(R.id.editTextTextMultiLine2)!!.text.toString()
+            qua = findViewById<EditText>(R.id.SelectQualification)!!.text.toString()
 
             if (qua!!.isNotEmpty() ) {
                 var intent = Intent(this, Coach_reg4::class.java)
 
-                sendInfo("http://192.168.31.127:5000/project?email=$email")
-                sendInfo("http://192.168.31.127:5000/project?pw=$pw")
-                sendInfo("http://192.168.31.127:5000/project?id=$id")
-                sendInfo("http://192.168.31.127:5000/project?first_name=$first_name")
-                sendInfo("http://192.168.31.127:5000/project?last_name=$last_name")
-                sendInfo("http://192.168.31.127:5000/project?username=$username")
-                sendInfo("http://192.168.31.127:5000/project?address=$address")
-                sendInfo("http://192.168.31.127:5000/project?gender=$gender")
-                sendInfo("http://192.168.31.127:5000/project?age=$age")
-                sendInfo("http://192.168.31.127:5000/project?exp=$exp")
-                sendInfo("http://192.168.31.127:5000/project?expertise=$expertise")
-                sendInfo("http://192.168.31.127:5000/project?intro=$intro")
-                sendInfo("http://192.168.31.127:5000/project?qua=$qua")
+                sendInfo("http://192.168.31.127:5000/project?email=$email&pw=$pw&id=$id&first_name=$first_name&last_name=$last_name&username=$username&address=$address&gender=$gender&age=$age&exp=$exp&expertise=$expertise&intro=$intro&qua=$qua")
 
-                /*startActivity(intent)*/
+                startActivity(intent)
             } else {
                 Toast.makeText(this@Coach_reg3, "Something is not completed. Please check", Toast.LENGTH_SHORT).show()
             }
@@ -76,41 +64,13 @@ class Coach_reg3 : AppCompatActivity() {
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET, url, null,
             Response.Listener { response ->
-                switchActivity(response)
+                println("added")
             },
             Response.ErrorListener { error ->
                 Log.e("MyActivity",error.toString())
             }
         )
         Volley.newRequestQueue(this).add(jsonObjectRequest)
-    }
-
-    fun switchActivity(jsonObj: JSONObject){
-        val users: JSONArray = jsonObj.getJSONArray("Users")
-
-        val studentList = arrayListOf<String>()
-        for (i in 0..users.length() - 1){
-            val `object`: JSONObject = users.getJSONObject(i)
-            val email = `object`.getString("email")
-            val pw = `object`.getString("pw")
-            val id = `object`.getString("id")
-            val first_name = `object`.getString("first_name")
-            val last_name = `object`.getString("last_name")
-            val username = `object`.getString("username")
-            val address = `object`.getString("address")
-            val gender = `object`.getString("gender")
-            val age = `object`.getString("age")
-            val exp = `object`.getString("exp")
-            val expertise = `object`.getString("expertise")
-            val intro = `object`.getString("intro")
-            val qua = `object`.getString("qua")
-            println(email)
-            println(pw)
-            println(id)
-            println(intro)
-            println(qua)
-//            studentList.add(users.get(i) as String)
-        }
     }
 
 }
